8fe29d1f4cec51e5a5f3075e00aba4b4e629c525
kitten-master <dgdev@naver.com>
2021-06-23 17:24:17 +0900

    address testnet fixed

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 // import { bsc, fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 import { injected, bsc } from '../connectors'
 // TODO
-export const ROUTER_ADDRESS = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
+export const ROUTER_ADDRESS = '0x7287C54aAAea2Fe5D195adD6825f58Ab2e13C581'
 
 // a list of tokens by chain
 type ChainTokenList = {
